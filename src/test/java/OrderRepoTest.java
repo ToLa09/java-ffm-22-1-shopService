1f2537e4916adc0f8a3145f8f00451b88cb64e10
ToLa09 <landgraf-tobias@web.de>
2022-10-07 16:45:54 +0200

    add surefire plugin to pom.xml

--- src/test/java/OrderRepoTest.java
+++ src/test/java/OrderRepoTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderRepoTest {
