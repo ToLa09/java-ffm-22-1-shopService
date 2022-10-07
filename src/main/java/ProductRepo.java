f762c67dce01bda404e5662b696cc9afa43af52b
ToLa09 <landgraf-tobias@web.de>
2022-10-07 11:28:00 +0200

    changes by christian

--- src/main/java/ProductRepo.java
+++ src/main/java/ProductRepo.java
@@ -2,5 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepo {
-    private List<Product> products = new ArrayList<>();
+
+    private Map<UUID,Product> products = new HashMap<>();
+
+    public ProductRepo() {
+    }
+
+    public ProductRepo(Map<UUID, Product> products) {
+        this.products = products;
+    }
+    public void addProduct(Product product){
+        UUID randomID = UUID.randomUUID();
+        products.put(randomID,product);
+
+    }
+
+    public Product getProduct(UUID id) throws IllegalArgumentException {
+        if (products.containsKey(id)) {
+            return products.get(id);
+        }
+        else {
+            throw new IllegalArgumentException("No product found with given ID");
+        }
+    }
+
+    public Map<UUID,Product> listProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "products=" + products +
+                '}';
+    }
+
+    public Product getProductBySerialID(int serialID) {
+        for (Product entry: products.values()) {
+            if (entry.getId() == serialID) {
+                return entry;
+            }
+        }
+        throw new RuntimeException("Kein Produkt mit dieser ID vorhanden");
+    }
+
+
 }
